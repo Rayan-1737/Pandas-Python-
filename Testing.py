8455ccce2a74c4d9da01731d8fd54f00d7fb4472
Rayan-1737 <apple@example.com>
2026-03-31 09:44:14 +0530

    Auto-sync: file changes

--- Testing.py
+++ Testing.py
@@ -1,17 +1,8 @@
 import pandas as pd
 
-# Testing auto-sync feature - March 30, 2026
-# Auto Git Sync Extension Test - 5 minute timer activated!
-
-# a = [1,2,3]
-
-# rayan = pd.Series(a)
-
-# rayan.index = index = ['X' , 'Y' , 'Z']
-
 rayan = pd.Series([10,50,20,30,40])
 
 rayan.index = index = ['A' , 'B' , 'C' , 'D' , 'E']
 
-print(rayan.values)
+print(rayan.head)
 
