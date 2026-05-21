446d7f2264acb2de30d99aa6cf5ba9a19837ea76
Rayan-1737 <apple@example.com>
2026-05-21 19:54:30 +0530

    Auto-sync: file changes

---
+++ Basics/Series.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+a = [1, 2, 3, 4, 5]
+
+b = pd.Series(a)
+
+print(b)
+
+
+
+# Creating Series:- 
+
+
+# A) From a List:- 
+
+import pandas as pd
+
+s = pd.Series([1, 2, 3, 4])
+
+print(s)
+
+
+
+
+
+
