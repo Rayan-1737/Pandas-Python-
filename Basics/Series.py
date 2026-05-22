29c8112d9d4a9478dfea267726ab274ffd459681
Rayan-1737 <apple@example.com>
2026-05-22 16:18:12 +0530

    Auto-sync: file changes

--- Basics/Series.py
+++ Basics/Series.py
@@ -162,6 +162,7 @@ print(s.shape)
 # Output:-  (3,)
 
 # Returns the dimensions of the Series. Returns the number of rows and columns  ex:-  (3 , 2) means 3 rows and 2 cloumns 
+# Used to understand the Size of the Data 
 
 # A Series is 1-dimensional.
 
